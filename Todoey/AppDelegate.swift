b51ced55ea3eb12603e6d85d0c01a11845c29204
Ben Cohen <ben.cohenvi@gmail.com>
2021-03-27 18:15:57 +0300

    Remove unnecessary CoreData code

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -17,39 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        self.saveContext()
-    }
-    
-    // MARK - Core Data stack
-    
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "DataModel")
-        container.loadPersistentStores { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
+        
+        do {
+            _ = try Realm()
+        } catch {
+            print("Error initialzing realm \(error)")
         }
         
-        return container
-    }()
-    
-    // Mark - Core Data saving support
-    
-    func saveContext() {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        return true
     }
-
 }
 
